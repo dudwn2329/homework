59b6981c9c871e12080bfe14f4a9653ac4a248ad
HyeN123 <42574414+HyeN123@users.noreply.github.com>
2019-04-28 21:06:52 +0900

    2차

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const path = require('path');
 const request = require('request');
 const cheerio = require('cheerio');
-const bodyParser = require('body-parser');  //여기
+const bodyParser = require('body-parser');
 
 let app = express();
 
@@ -13,8 +13,8 @@ app.set('views', path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.json()); //미들웨어로 바디파서를 사용함. //여기
-app.use(bodyParser.urlencoded({extended:true})); //여기
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function(req, res){
 
@@ -29,7 +29,7 @@ app.get('/', function(req, res){
             list.push(msg);
         }
 
-        res.render('dunfa', {msg:'던파 캐릭터 이름', list:list});
+        res.render('main', {msg:'던파 캐릭터 이름', list:list});
     });
 });
 
@@ -37,5 +37,3 @@ let server = http.createServer(app);
 server.listen(app.get('port'), function () {
     console.log(`Express 엔진이 ${app.get('port')}에서 실행중`);
 });
-
-
